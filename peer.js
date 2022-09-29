5b7934109d75c27b63c982ab74fe034f99b6a594
Tomás Ciccola <tomas.ciccola@carejourney.com>
2022-09-29 13:49:27 -0300

    extension working :)

--- peer.js
+++ peer.js
@@ -5,32 +5,41 @@ const Corestore = require("corestore");
 const storePath = process.argv.slice(2)[0];
 // names of cores to create or open
 const coreNames = process.argv.slice(3);
+
 // hardcored topic
 const topic = Buffer.alloc(32).fill("my-topic");
+const coreName = Buffer.alloc(32).fill("example-master-core");
 
 const swarm = new Hyperswarm();
 const store = new Corestore(storePath);
+const masterCore = store.get({ key: coreName });
 
-swarm.on("connection", (conn) => {
+let peer;
+
+swarm.on("connection", async (conn) => {
   console.log("we are connected!");
-
-  coreNames.forEach(async (name) => {
-    const core = store.get({ name });
-    await core.ready();
-    const key = core.key;
-    await core.append(Buffer.from(Math.random() * name.length));
-    conn.write(key);
+  await masterCore.ready();
+  // const key = masterCore.key;
+  const extension = masterCore.registerExtension("replicator", {
+    encoding: "binary",
+    onmessage: (msg, peer) => {
+      console.log("new message from extension!");
+      console.log("key:", msg.toString("hex"));
+      store.get({ key: msg });
+    },
   });
 
-  conn.on("data", (d) => {
-    console.log("new key from peer!");
-    console.log(d.toString("hex"));
-    store.get({ key: d });
+  masterCore.on("peer-add", (peer) => {
+    coreNames.forEach(async (name) => {
+      const core = store.get({ name });
+      await core.ready();
+      await core.append(Buffer.from(String(Math.random() * name.length)));
+      const key = core.key;
+      console.log(key.toString("hex"));
+      extension.send(key, peer);
+    });
   });
-
-  // replicate store from peer
-  // this breaks because we are replicating the store and sharing keys via the same socket
-  // store.replicate(conn);
+  store.replicate(conn);
 });
 
 swarm.join(topic);
